dd733e1dc209279a186ac7258ef934085b37c0b6
kpfuller28 <kpfuller28@gmail.com>
2024-12-13 15:18:59 -0500

    pythonanywhere edit

--- app/__init__.py
+++ app/__init__.py
@@ -12,5 +12,8 @@ def create_app():
   app.secret_key = os.getenv('SECRET_KEY')
   app.config['API_URL'] = os.getenv('API_URL')
   app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
+  print('--------------------------------------------')
+  print(app.config['SQLALCHEMY_DATABASE_URI'])
+  print('--------------------------------------------')
   db.init_app(app)
   return app
