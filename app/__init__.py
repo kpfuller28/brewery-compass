06db054034ad4b73563928219db3eaab7c749033
kpfuller28 <kpfuller28@gmail.com>
2024-12-13 22:23:16 -0500

    more edits

--- app/__init__.py
+++ app/__init__.py
@@ -13,10 +13,6 @@ def create_app():
   app.secret_key = os.getenv('SECRET_KEY')
   app.config['API_URL'] = os.getenv('API_URL')
   app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI')
-  print('--------------------------------------------')
-  print(os.name)
-  print(os.getenv('DB_URI'))
-  print('--------------------------------------------')
   db.init_app(app)
 
   with app.app_context():
